ade980985b8a7b0f8076c9c40d37f1650b349f4f
vans163 <vans554@gmail.com>
2024-12-30 18:41:13 -0500

    fix Base58.decode with leading zeros

--- lib/util.ex
+++ lib/util.ex
@@ -57,22 +57,33 @@ end
 
 defmodule Base58 do
   @alnum ~c(123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz)
+  @one32 :binary.copy("1", 32)
+  @zero32 :binary.copy(<<0>>, 32)
 
   def encode(e) when is_integer(e) or is_float(e) or is_atom(e), do: encode("#{e}")
   # see https://github.com/dwyl/base58/issues/5#issuecomment-459088540
   def encode(<<0, binary::binary>>), do: "1" <> encode(binary)
   def encode(""), do: ""
   # see https://github.com/dwyl/base58/pull/3#discussion_r252291127
-  def encode(binary), do: encode(:binary.decode_unsigned(binary), "")
-  def encode(0, acc), do: acc
-  def encode(n, acc), do: encode(div(n, 58), <<Enum.at(@alnum, rem(n, 58))>> <> acc)
+  def encode(binary) when is_binary(binary), do: encode_1(:binary.decode_unsigned(binary, :big), "")
+  def encode_1(0, acc), do: acc
+  def encode_1(n, acc), do: encode_1(div(n, 58), <<Enum.at(@alnum, rem(n, 58))>> <> acc)
+
+  defp decode_leading_zero(<<"1", tail::binary>>, zeros), do: decode_leading_zero(tail, zeros + 1)
+  defp decode_leading_zero(binary, zeros), do: {binary, zeros}
 
   def decode(""), do: "" # return empty string unmodified
   def decode("\0"), do: "" # treat null values as empty
-  def decode(binary), do: decode(binary, 0)
-  def decode("", acc), do: :binary.encode_unsigned(acc)
-  def decode(<<head, tail::binary>>, acc),
-    do: decode(tail, acc * 58 + Enum.find_index(@alnum, &(&1 == head)))
+  def decode(@one32), do: @zero32
+  def decode(binary) when is_binary(binary) do
+    {binary, zeros} = decode_leading_zero(binary, 0)
+    decoded = decode_1(binary, 0)
+    :binary.copy(<<0>>, zeros) <> decoded
+  end
 
-  def decode_to_int(encoded), do: encoded |> decode() |> String.to_integer()
+  def decode_1("", acc), do: :binary.encode_unsigned(acc, :big)
+  def decode_1(<<head, tail::binary>>, acc),
+    do: decode_1(tail, acc * 58 + Enum.find_index(@alnum, &(&1 == head)))
+
+  def decode_to_int(encoded), do: encoded |> decode() |> :binary.decode_unsigned(:big)
 end
